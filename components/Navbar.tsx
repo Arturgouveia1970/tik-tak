cf2834b961d355dd24a866d8abb6c4239967c01e
Artur <artgouveiag@gmail.com>
2023-11-14 19:47:05 +0100

    problems with jwt

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,12 @@ import { useRouter } from 'next/router';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
@@ -17,10 +19,21 @@ const Navbar = () => {
             className="cursor-pointer"
             src={Logo}
             alt="TIKTIK"
-            Layout="responsive"
+            layout="responsive"
           />    
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
     // <h1>navbar</h1>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import '../styles/globals.css';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -14,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if(isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -24,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
